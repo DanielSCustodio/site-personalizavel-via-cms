5a8b2c85a43dae5a41ece341af060c3850b6cf29
Daniel Custodio <play.daniel@hotmail.com>
2022-10-29 16:02:53 -0300

    feat: Global style

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { AppProps } from 'next/app';
+import '../styles/global.sass';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
