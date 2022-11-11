7e44b50613cceeeb065dd83b9346bf9d98bfb5ad
Daniel Custódio <play.daniel@hotmail.com>
2022-11-11 16:51:45 -0300

    feat: Main session creation - In progress

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,15 @@
+import Head from 'next/head';
 import React from 'react';
+import Main from '../templates/Main';
+import styles from '../styles/home.module.sass';
 
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <>
+      <Head>
+        <title>Home - Construtor de sites</title>
+      </Head>
+      <Main />
+    </>
+  );
 }
---
+++ src/templates/Main/index.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import styles from './styles.module.sass';
+
+export default function Main() {
+  return (
+    <main className={styles.container}>
+      <section className={styles.callAction}>
+        <h2>
+          O Melhor<span>&</span>
+        </h2>
+        <h2>
+          <span>+</span>Criativo
+        </h2>
+        <h2>Construtor de Sites</h2>
+        <p className={styles.text}>
+          Aumente a credibilidade da sua marca. <br />
+          Saia na frente dos seus concorrentes, construa seu site agora!
+        </p>
+        <button>Começar a construir &nbsp;&nbsp;➜</button>
+        <section className={styles.numbers}>
+          <div>
+            <h3>+30</h3>
+            <p>Clientes satisfeitos</p>
+          </div>
+          <div>
+            <h3>+70</h3>
+            <p>Cases de sucesso</p>
+          </div>
+        </section>
+      </section>
+      <section className={styles.banner}>
+        <img src="/images/banner.png" alt="" />
+      </section>
+    </main>
+  );
+}
