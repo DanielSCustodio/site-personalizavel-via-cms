e334f23a7f83b4760714c916113ae0e3a4ce8906
Daniel Custódio <play.daniel@hotmail.com>
2022-12-01 17:11:50 -0300

    Delete prismic.ts

--- src/service/prismic.ts
+++
@@ -1,8 +0,0 @@
-import Prismic from '@prismicio/client';
-
-export function getPrismicCliente(req?: unknown) {
-  const prismic = Prismic.client('https://customwebsite.prismic.io/api/v2', {
-    req,
-  });
-  return prismic;
-}
