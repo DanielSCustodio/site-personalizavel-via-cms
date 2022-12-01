d42f1607d03b94405b96846d15448656b0a2bd8d
Daniel Custodio <play.daniel@hotmail.com>
2022-12-01 17:25:04 -0300

    feat: Prismic Configuration - Environment Variables

---
+++ src/service/prismic.ts
@@ -0,0 +1,8 @@
+import Prismic from '@prismicio/client';
+
+export function getPrismicCliente(req?: unknown) {
+  const prismic = Prismic.client(process.env.PRISMIC_ENDPOINT, {
+    req,
+  });
+  return prismic;
+}
