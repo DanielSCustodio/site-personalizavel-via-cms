b3c3f517bf0853a7a40403dde823405049ba6da4
Daniel Custodio <play.daniel@hotmail.com>
2022-12-08 20:06:00 -0300

    feat: Fetch api - banner section

--- src/templates/Banner/index.tsx
+++ src/templates/Banner/index.tsx
@@ -1,20 +1,20 @@
 import React from 'react';
+import Prismic from '@prismicio/client';
+import { RichText } from 'prismic-dom';
+import { getPrismicClient } from '../../service/prismic';
+import { GetStaticProps } from 'next';
+
 import styles from './styles.module.sass';
 
 export default function Banner() {
   return (
     <section className={styles.container}>
       <section className={styles.callAction}>
-        <h2>
-          O Melhor<span>&</span>
-        </h2>
-        <h2>
-          <span>+</span>Criativo
-        </h2>
+        <h2>O Melhor e mais Criativo</h2>
         <h2>Construtor de Sites</h2>
         <p className={styles.text}>
-          Aumente a credibilidade da sua marca. <br />
-          Saia na frente dos seus concorrentes, construa seu site agora!
+          Aumente a credibilidade da sua marca. Saia na frente dos seus
+          concorrentes, construa seu site agora!
         </p>
         <button>Começar a construir &nbsp;&nbsp;➜</button>
         <section className={styles.numbers}>
